a87b0d69a1755f487a98c807dcdaac04536dd2dc
Qifeng <qifeng.bai@csiro.au>
2017-11-06 12:40:54 +1100

    funding to int

--- web-app/js/projects.js
+++ web-app/js/projects.js
@@ -328,7 +328,7 @@ function ProjectViewModel(project, isUserEditor) {
     self.funding = ko.computed(function(){
         var total = 0;
         ko.utils.arrayForEach(self.fundings() ,function(funding){
-            total += parseFloat(funding.fundingSourceAmount(),2);
+            total += parseInt(funding.fundingSourceAmount());
         })
         return total;
     }).extend({currency:{currencySymbol:"AUD $ "}})
