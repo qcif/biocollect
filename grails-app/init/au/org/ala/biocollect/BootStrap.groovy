9ac46e3780e23edca5e9baa71f49701a15b14069
Qifeng <qifeng.bai@csiro.au>
2019-05-22 11:29:28 +1000

    Optimise config

--- grails-app/init/au/org/ala/biocollect/BootStrap.groovy
+++ grails-app/init/au/org/ala/biocollect/BootStrap.groovy
@@ -2,8 +2,10 @@ package au.org.ala.biocollect
 
 import grails.converters.JSON
 import net.sf.json.JSONNull
+import grails.core.GrailsApplication
 
 class BootStrap {
+    GrailsApplication grailsApplication
 
     def init = { servletContext ->
         JSON.createNamedConfig("nullSafe", { cfg ->
